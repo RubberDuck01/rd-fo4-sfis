b479027f7a44ac5ba6691b1fb3fd4f25cff8a305
RubberDuck01 <spehar.luka@hotmail.com>
2024-01-21 20:23:27 +0100

    Added initial main window

---
+++ SFIS/source/gui/main_window.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QLabel
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Rubber Duck's Simple FallUI Item Sorter")
+        self.lblWelcome = QLabel("Welcome to Rubber Duck's Simple FallUI Item Sorter", self)
+        self.lblWelcome.setGeometry(50, 20, 300, 15)
+        self.setGeometry(100, 100, 400, 300)
+
+        self.btnLoad = QPushButton("Load CSV", self)
+        self.btnLoad.setGeometry(50, 50, 100, 30)
+
+        self.button2 = QPushButton("Button 2", self)
+        self.button2.setGeometry(50, 100, 100, 30)
+
+        self.textbox1 = QLineEdit(self)
+        self.textbox1.setGeometry(200, 50, 150, 30)
+
+        self.textbox2 = QLineEdit(self)
+        self.textbox2.setGeometry(200, 100, 150, 30)
+
+
+# if __name__ == "__main__":
+#     app = QApplication(sys.argv)
+#     window = MainWindow()
+#     window.show()
+#     sys.exit(app.exec())
--- SFIS/source/main.py
+++ SFIS/source/main.py
@@ -1,1 +1,10 @@
-print("Welcome to SFIS!")
+import sys
+from PyQt6.QtWidgets import QApplication
+from gui.main_window import MainWindow
+
+if __name__ == "__main__":
+    print("Welcome to SFIS!")
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
